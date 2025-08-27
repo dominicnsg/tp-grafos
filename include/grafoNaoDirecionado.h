ddd7b0caa589134480dc207b5ce4381a1c86fed5
Deb Luiza <99653788+DebLuiza@users.noreply.github.com>
2025-08-27 18:48:55 -0300

    Revert "Merge branch 'ponderado' of https://github.com/dominicnsg/tp-grafos into ponderado"
    This reverts commit 677aa50630be51a4ed9ecc60ff4711b15bce2906, reversing
    changes made to 8d47742482ea4d8395a964c86469f4f3f2eb4e94.

--- include/grafoNaoDirecionado.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "grafoDirecionado.h"
-
-class GrafoNaoDirecionado : public GrafoDirecionado {
-public:
-    explicit GrafoNaoDirecionado(int vertices);
-    void adicionarAresta(int v1, int v2) override;
-    void imprimirGrafo() const override;
-};
